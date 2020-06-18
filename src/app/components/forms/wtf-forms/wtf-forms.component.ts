d0461ffa3586dbbf6f765331f5768b39c7df90e5
martinboykov <martinboykov@mail.bg>
2020-06-18 20:33:50 +0300

    added custom file

--- src/app/components/forms/wtf-forms/wtf-forms.component.ts
+++ src/app/components/forms/wtf-forms/wtf-forms.component.ts
@@ -44,6 +44,12 @@ export class WtfFormsComponent implements OnInit, AfterViewInit {
   </select>
 </div>
     `,
+    file: `
+<label class="file">
+  <input type="file" id="file" aria-label="File browser example">
+  <span class="file-custom"></span>
+</label>
+    `,
   };
 
   css = {
@@ -290,6 +296,63 @@ background-color: #ddd;
   }
 }
 `,
+    file: `
+.file {
+  position: relative;
+  display: inline-block;
+  cursor: pointer;
+  height: 3.5rem;
+}
+.file input {
+  min-width: 14rem;
+  margin: 0;
+  filter: alpha(opacity=0);
+  opacity: 0;
+}
+.file-custom {
+  position: absolute;
+  top: 0;
+  right: 0;
+  left: 0;
+  z-index: 5;
+  height: 3.5rem;
+  padding: 0.5rem 1rem;
+  line-height: 1.5;
+  color: #555;
+  background-color: #fff;
+  border: 0.075rem solid #ddd;
+  border-radius: 0.25rem;
+  box-shadow: inset 0 0.2rem 0.4rem rgba(0, 0, 0, 0.05);
+  -webkit-user-select: none;
+  -moz-user-select: none;
+  -ms-user-select: none;
+  user-select: none;
+}
+.file-custom:after {
+  content: "Choose file...";
+}
+.file-custom:before {
+  position: absolute;
+  top: -0.075rem;
+  right: -0.075rem;
+  bottom: -0.075rem;
+  z-index: 6;
+  display: block;
+  content: "Browse";
+  height: 3.5rem;
+  padding: 0.5rem 1rem;
+  line-height: 1.5;
+  color: #555;
+  background-color: #eee;
+  border: 0.075rem solid #ddd;
+  border-radius: 0 0.25rem 0.25rem 0;
+}
+
+/* Focus */
+.file input:focus ~ .file-custom {
+  box-shadow: 0 0 0 0.075rem #fff, 0 0 0 0.2rem #0074d9;
+}
+`,
   };
   constructor(private prismService: PrismService) {}
 
@@ -298,10 +361,15 @@ background-color: #ddd;
       checkbox: this.prismService.highlight(this.html.checkbox),
       radio: this.prismService.highlight(this.html.radio),
       select: this.prismService.highlight(this.html.select),
+      file: this.prismService.highlight(this.html.file),
     };
     this.cssToken = {
-      checkboxAndRadio: this.prismService.highlight(this.css.checkboxAndRadio, 'css'),
+      checkboxAndRadio: this.prismService.highlight(
+        this.css.checkboxAndRadio,
+        'css'
+      ),
       select: this.prismService.highlight(this.css.select, 'css'),
+      file: this.prismService.highlight(this.css.file, 'css'),
     };
   }
 
